142d94c67b915abdb26b1eccf751a36d3891b9cc
Sergey Potapov <potapovss2002@gmail.com>
2023-05-03 15:07:22 +0300

    Update

--- firstrun.cmd
+++ firstrun.cmd
@@ -14,13 +14,12 @@ git pull origin master
 python -m venv venv
 call venv\Scripts\activate.bat
 
-pip install torch==1.12.1+cu116 torchvision==0.13.1+cu116 --extra-index-url https://download.pytorch.org/whl/cu116
-pip install --use-pep517 --upgrade -r requirements.txt
-pip install -U -I --no-deps https://github.com/C43H66N12O12S2/stable-diffusion-webui/releases/download/f/xformers-0.0.14.dev0-cp310-cp310-win_amd64.whl
-pip uninstall -y huggingface-hub
-pip install -U -I huggingface-hub==0.13.3
+pip install torch==2.0.0+cu118 torchvision==0.15.1+cu118 --extra-index-url https://download.pytorch.org/whl/cu118
+pip install --use-pep517 -U -r requirements.txt
+pip install -U xformers==0.0.17
+pip install https://huggingface.co/r4ziel/xformers_pre_built/resolve/main/triton-2.0.0-cp310-cp310-win_amd64.whl
 
-echo Installing bitsandbytes and CUDNN
+echo Installing bitsandbytes
 python firstrun.py
 
 del firstrun.* & webui.cmd
