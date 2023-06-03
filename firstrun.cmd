ed475f428bf76f859c2d5e2174c3532d7fc77f65
Sergey Potapov <potapovss2002@gmail.com>
2023-06-03 19:15:19 +0300

    Update

--- firstrun.cmd
+++ firstrun.cmd
@@ -14,9 +14,9 @@ git pull origin master
 python -m venv venv
 call venv\Scripts\activate.bat
 
-pip install torch==2.0.0+cu118 torchvision==0.15.1+cu118 --extra-index-url https://download.pytorch.org/whl/cu118
+pip install torch==2.0.1+cu118 torchvision==0.15.2+cu118 --extra-index-url https://download.pytorch.org/whl/cu118
 pip install --use-pep517 -U -r requirements.txt
-pip install -U xformers==0.0.17
+pip install -U xformers==0.0.20
 pip install https://huggingface.co/r4ziel/xformers_pre_built/resolve/main/triton-2.0.0-cp310-cp310-win_amd64.whl
 
 echo Installing bitsandbytes
