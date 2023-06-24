44ab23e250baad9a84215baceb26b9cefa0ad2d5
Sergey Potapov <potapovss2002@gmail.com>
2023-06-24 17:00:25 +0300

    Update

--- firstrun.cmd
+++ firstrun.cmd
@@ -14,14 +14,7 @@ git pull origin master
 python -m venv venv
 call venv\Scripts\activate.bat
 
-pip install torch==2.0.1+cu118 torchvision==0.15.2+cu118 --extra-index-url https://download.pytorch.org/whl/cu118
-pip install --use-pep517 -U -r requirements_windows_torch2.txt
-pip install -U xformers==0.0.20
-pip install https://huggingface.co/r4ziel/xformers_pre_built/resolve/main/triton-2.0.0-cp310-cp310-win_amd64.whl
-
-echo Installing bitsandbytes
-python firstrun.py
-
+pip install --use-pep517 -U -r firstrun.txt
 del firstrun.* & webui.cmd
 
 pause
